dff024bc85553fd9c27ff2ffbdf5f44251cd5772
cokenol <ukenster@gmail.com>
2022-04-18 22:30:57 +0800

    Able to set pixel width and height of bitmapimage but unable to set uniform grid column number

--- VideoThumbnail/MainWindow.xaml.cs
+++ VideoThumbnail/MainWindow.xaml.cs
@@ -19,13 +19,14 @@ using Microsoft.WindowsAPICodePack.Shell;
 using System.Diagnostics;
 using NReco.VideoConverter;
 using System.Drawing.Imaging;
+using System.ComponentModel;
 
 namespace VideoThumbnail
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
         public MainWindow()
         {
@@ -38,39 +39,54 @@ namespace VideoThumbnail
                 FileLocation.Text = openFileDialog.FileName;
             //FileLocation.Text = File.ReadAllText(openFileDialog.FileName);
         }
-
-        private void btnExecute(object sender, RoutedEventArgs e)
+        private async void btnExecute(object sender, RoutedEventArgs e)
+        {
+            ExecuteFileButton.Content = "Doing...";
+            await Task.Delay(1);
+            btnExecute_Do();
+            ExecuteFileButton.Content = "Execute";
+        }
+        private void btnExecute_Do()
         {
             //method 1
             //ShellFile shellFile = ShellFile.FromFilePath(FileLocation.Text);
             //Bitmap bm = shellFile.Thumbnail.Bitmap;
             //images.ItemsSource = GetListOfImages(bm, 20);
 
-            //method2
-            ExecuteFileButton.Content = "Doing...";
+            //method2            
             images.ItemsSource = GetListOfImages2(FileLocation.Text, 16);
-            ExecuteFileButton.Content = "Execute";
+            
         }
         public static List<BitmapImage> GetListOfImages2(string video, int times)
         {
-            List<BitmapImage> images = new List<BitmapImage>();
+            List<BitmapImage> imagesL = new List<BitmapImage>();
             FFMpegConverter ffMpeg = new FFMpegConverter();
             var ffProbe = new NReco.VideoInfo.FFProbe();
             var videoInfo = ffProbe.GetMediaInfo(video);
             double duration = videoInfo.Duration.TotalSeconds;
+            int height = videoInfo.Streams[0].Height;
+            int width = videoInfo.Streams[0].Width;
+            //Console.WriteLine("W= {0} x H ={1}", width, height);
+            ImageChecker wh = new ImageChecker { ImageWidth = 200, ImageHeight = 150 };
+            wh.CheckedWidthHeight(height, width);
+            int displayWidth = wh.ImageWidth;
+            int displayHeight = wh.ImageHeight;
+
             for (float i = 0; i < times; i++)
             {
                 //Console.WriteLine("doing {0}", i);
                 var thumbJpegStream = new MemoryStream();
                 float position = (float)duration * i / times;
-                Console.WriteLine(position);
+                //Console.WriteLine(position);
                 ffMpeg.GetVideoThumbnail(video, thumbJpegStream, position);
-                images.Add(StreamToImage(thumbJpegStream));
-                //Console.WriteLine(images.Count);
+                //Console.WriteLine(StreamToImage(thumbJpegStream));
+                var img = StreamToImage(thumbJpegStream, displayHeight, displayWidth);
+                //wh.SetWidthHeight(img);
+                imagesL.Add(img);
             }
-            return images;
+            return imagesL;
         }
-        private static BitmapImage StreamToImage(MemoryStream stream)
+        private static BitmapImage StreamToImage(MemoryStream stream,int height,int width)
         {
             // assumes that the streams position is at the beginning
             // for example if you use a memory stream you might need to point it to 0 first
@@ -80,6 +96,8 @@ namespace VideoThumbnail
             stream.Seek(0, SeekOrigin.Begin);
             image.CacheOption = BitmapCacheOption.OnLoad;
             image.StreamSource = stream;
+            image.DecodePixelHeight = height;
+            image.DecodePixelWidth = width;
             image.EndInit();
 
             image.Freeze();
@@ -121,6 +139,64 @@ namespace VideoThumbnail
         {
             var ms = new MemoryStream(File.ReadAllBytes(path));
             return (Bitmap)System.Drawing.Image.FromStream(ms);
+        }        
+    }
+
+    public class ImageChecker : INotifyPropertyChanged //Hold control and hit period to add the using for this
+    {
+        private int _imageWidth;
+        public int ImageWidth
+        {
+            get { return _imageWidth; }
+            set { _imageWidth = value; OnPropertyChanged("ImageWidth"); }
+        }
+
+        private int _imageHeight;
+        public int ImageHeight
+        {
+            get { return _imageHeight; }
+            set { _imageHeight = value; OnPropertyChanged("ImageHeight"); }
+        }
+        private int _columnNums = 8;
+        public int ColumnNums
+        {
+            get { return _columnNums; }
+            set { _columnNums = value; OnPropertyChanged("ColumnNums"); }
+        }
+        public void CheckedWidthHeight(int height, int width)
+        {
+            if (height > width)
+            {
+                Console.WriteLine("Height {0} > Width {1} ",height, width);
+                _columnNums = 5;
+                _imageHeight = height / 10;
+                _imageWidth = width / 10;
+                Console.WriteLine("{0} {1}",ImageHeight.ToString(), ImageWidth.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Height {0} < Width {1} ", height, width);
+                _columnNums = 4;
+                _imageHeight = height / 10;
+                _imageWidth = width / 10;
+                Console.WriteLine("{0} {1}", ImageHeight.ToString(), ImageWidth.ToString());
+            }
+        }
+        public void SetWidthHeight(BitmapImage img)
+        {
+            img.DecodePixelHeight = ImageHeight;
+            img.DecodePixelWidth = ImageWidth;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(String prop)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
         }
     }
+
 }
